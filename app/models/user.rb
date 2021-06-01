bfd34023e71dced5624ca60c70d5fd9e137301ea
mikannsamurai <83692341+mikannsamurai@users.noreply.github.com>
2021-06-01 18:00:36 +0900

    テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,              presence: true
-  validates :email,                 presence: true
   validates :password,              format: { with: /\A[a-z0-9]+\z/i  }
   validates :password_confirmation, presence: true
   validates :first_name_full_width, presence: true, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
@@ -15,3 +14,4 @@ class User < ApplicationRecord
   validates :birthday,              presence: true
   
 end
+
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation { password }
+    first_name_full_width {"山田"}
+    last_name_full_width  {"太郎"}
+    first_name_katakana   {"ヤマダ"}
+    last_name_katakana    {"タロウ"}
+    birthday              {Faker::Date.between(from: '1930/01/01', to: '2015/12/31')}
+
+  end
+end
