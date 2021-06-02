f3d1ddd35efb9a32e7df5ebdea133e19ca43201b
mikannsamurai <83692341+mikannsamurai@users.noreply.github.com>
2021-06-02 16:53:44 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,17 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_burden
 
   has_one_attached :image
+
+  with_options resence: true do
+  validates :name
+  validates :text
+  validates :price
+  end
+  with_options numericality: { onther_than: 1 } do
+  validates :category_id
+  validates :estimated_shipping_date
+  validates :prefecture
+  validates :product_condition
+  validates :shipping_fee_burden
+  end
 end
