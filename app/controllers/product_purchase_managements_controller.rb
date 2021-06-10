b5083c244a6082ab5bdff5e688cdc3d16d26b3cc
mikannsamurai <83692341+mikannsamurai@users.noreply.github.com>
2021-06-10 12:11:12 +0900

    コントローラー何にフォームの情報を保存できる記述

--- app/controllers/product_purchase_managements_controller.rb
+++ app/controllers/product_purchase_managements_controller.rb
@@ -6,5 +6,17 @@ class ProductPurchaseManagementsController < ApplicationController
   end
 
   def create
+    @product_purchase_management = ProductPurchaseManagement.new(product_purchase_management_params)
+    if @product_purchase_management.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def product_purchase_management_params
+    params.require(order_form).permit(:municipalities, :address, :postal_coke, :phone_number, :prefecture_id)
+                                      .merge(user_id: current_user.id )
   end
 end
