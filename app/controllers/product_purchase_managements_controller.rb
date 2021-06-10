8db642afc69dd62881d6922a25016bfb0d8e2271
mikannsamurai <83692341+mikannsamurai@users.noreply.github.com>
2021-06-10 11:46:49 +0900

    item情報を渡して購入画面へ遷移

--- app/controllers/product_purchase_managements_controller.rb
+++ app/controllers/product_purchase_managements_controller.rb
@@ -1,4 +1,6 @@
 class ProductPurchaseManagementsController < ApplicationController
   def index
+    product_purchase_management = ProductPurchaseManagement.new
+    @item = Item.find(params[:item_id])
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -6,7 +6,7 @@ class OrderForm
   with_options presence: true do
     with_options format: {with: /\A([ぁ-んァ-ン一-龥々]|ー)+\z/} do
       validates :municipalities
-      validates :address
+      validates :address, format: {with: /\A[0]{1}\d{9,10}\z/}
     end
   validates :user_id
   validates :postal_coke, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid.Include hyphen(-)"}
@@ -15,7 +15,8 @@ class OrderForm
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   
   def save
-    product_purchase_management = Product_purchase_management.create(item: item, user_id: user_id)
+    product_purchase_management = ProductPurchaseManagement.create(item: item, user_id: user_id)
     Buyer.create(municipalities: municipalities, address: address, postal_coke: postal_coke, phone_number: phone_number, 
                  prefecture_id: prefecture_id, product_purchase_management_id: product_purchase_management.id )
+  end
 end
